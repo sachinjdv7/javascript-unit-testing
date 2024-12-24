1ace35834ad4b20b32e7cde55f836a0f074b9bba
sachinjadhav <sachinjdv7@gmail.com>
2024-12-24 16:07:38 +0530

    write test cases for geCoupons and calculateDiscount

--- src/test/intro.test.js
+++ src/test/intro.test.js
@@ -1,5 +1,5 @@
 import { it, expect, describe } from 'vitest';
-import { getCoupons } from '../core';
+import { calculateDiscount, getCoupons } from '../core';
 
 describe('getCoupons', () => {
   it('should return array of coupons', () => {
@@ -25,5 +25,28 @@ describe('getCoupons', () => {
       expect(coupon.discount).toBeGreaterThan(0);
       expect(coupon.discount).toBeLessThan(1);
     });
+  });
+});
+
+describe('calculateDiscount', () => {
+  it('should return discounted price if given valid code', () => {
+    expect(calculateDiscount(10, 'SAVE10')).toBe(9);
+    expect(calculateDiscount(10, 'SAVE20')).toBe(8);
+  });
+
+  it('should handle non-numeric price', () => {
+    expect(calculateDiscount('10', 'SAVE10')).toMatch(/invalid/i);
+  });
+
+  it('should handle negative price', () => {
+    expect(calculateDiscount(-10, 'SAVE10')).toMatch(/invalid/i);
+  });
+
+  it('should handle non-string discount code', () => {
+    expect(calculateDiscount(10, 10)).toMatch(/invalid/i);
+  });
+
+  it('should handle invalid discount code', () => {
+    expect(calculateDiscount(10, 'INVALID')).toBe(10);
   });
 });
